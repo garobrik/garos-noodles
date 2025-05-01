1a846c250b626379dbf36bc8b5e62d26b394ba51
shrouxm <mushrouxm@gmail.com>
2025-04-30 18:32:31 -0700

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 
 export default defineConfig({
+  base: '/garos-noodles/',
   plugins: [
     vike({}),
     {

c642d4bb46977ed9ee4e1e33e1417e58d40299da
shrouxm <mushrouxm@gmail.com>
2025-04-30 18:19:48 -0700

    fix: build

--- components/Noodles.tsx
+++ components/Noodles.tsx
@@ -1,4 +1,4 @@
-import { getGlobalContextSync } from 'vike';
+import { getGlobalContextSync } from 'vike/server';
 
 type Noodle = {
   frontmatter: {
@@ -13,14 +13,18 @@ const noodlePages = Object.entries(pages).filter(([path]) =>
 );
 const noodles = await Promise.all(
   noodlePages.map(async ([path, page]) => ({
-    noodle: ((await import(`..${path}/+Page.mdx`)) as Noodle).frontmatter,
+    noodle: (
+      (await import(
+        `../pages/(noodle)/${path.replace('/pages/(noodle)/', '')}/+Page.mdx`
+      )) as Noodle
+    ).frontmatter,
     page,
   })),
 );
 
 export const Noodles = () => {
   return noodles.map(({ noodle, page: { route } }) => (
-    <a className="no-underline" href={route as string}>
+    <a key={route as string} className="no-underline" href={route as string}>
       <h3 className="underline">{noodle.title}</h3>
       <p>{noodle.description}</p>
     </a>
--- pages/+Layout.tsx
+++ pages/+Layout.tsx
@@ -3,11 +3,11 @@ import './style.css';
 export default function LayoutDefault({ children }: React.PropsWithChildren) {
   return (
     <div className="m-auto max-w-[60rem] px-4">
-      <nav className="flex flex-wrap items-baseline justify-between gap-x-4 py-4">
-        <a href="/" className="text-3xl xs:text-4xl font-semibold mr-2">
+      <nav className="flex flex-wrap items-baseline justify-between gap-x-12 py-4">
+        <a href="/" className="text-2xl sm:text-4xl font-semibold">
           garo's noodle garden
         </a>
-        <a className="text-xl font-semibold" href="/about">
+        <a className="ml-auto text-lg font-semibold" href="/about">
           about
         </a>
       </nav>
